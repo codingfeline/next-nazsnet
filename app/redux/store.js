0a36e1ab44e0c00233673bc3c624185d249d2c7b
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-09 05:47:17 +0100

    redux

---
+++ app/redux/store.js
@@ -0,0 +1,12 @@
+'use client'
+
+import { configureStore } from '@reduxjs/toolkit'
+import authSlice from './features/auth/authSlice'
+import counterSlice from './features/counter/counterSlice'
+
+export const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    counter: counterSlice,
+  },
+})

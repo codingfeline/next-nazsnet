275cb4993604763938ad77215b1dd0746f3a309b
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-11 22:07:44 +0100

    journal

--- redux/store.js
+++ redux/store.js
@@ -3,10 +3,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from './features/auth/authSlice'
 import counterSlice from './features/counter/counterSlice'
+import journalSlice from './features/journal/journalSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     counter: counterSlice,
+    journal: journalSlice,
   },
 })

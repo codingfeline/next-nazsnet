db6d344a193787ccb33dcdfd63760d3cba5974f7
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-13 23:22:10 +0100

    reuse Dialog modal

--- app/page.js
+++ app/page.js
@@ -1,16 +1,20 @@
 'use client'
-
-import {
-  useEffect,
-  useDispatch,
-  axios,
-  setJournal,
-  useSelector,
-} from '@components'
+// prettier-ignore
+import { useEffect, useState,  useDispatch,axios,  setJournal, useSelector, Modal } from '@components'
 
 const page = () => {
   const counter = useSelector(state => state.counter.value)
   const dispatch = useDispatch()
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openDialog = () => {
+    setIsOpen(true)
+  }
+
+  const closeDialog = () => {
+    setIsOpen(false)
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,6 +36,16 @@ const page = () => {
         <input type="text" onChange={e => setQuery(e.target.value)} />
         {counter}
       </div>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          onClick={openDialog}
+          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        >
+          Open dialog
+        </button>
+      </div>
+      <Modal isOpen={isOpen} onClose={closeDialog} />
     </>
   )
 }

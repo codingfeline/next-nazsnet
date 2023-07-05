2eb256a39c09a398567bbe18d4eac81696f29e6b
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-05 22:05:12 +0100

    photo gallery

---
+++ app/photos/page.jsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { current } from '@reduxjs/toolkit'
+import React, { useState } from 'react'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
+import { RxDotFilled } from 'react-icons/rx'
+
+const page = () => {
+  const slides = [
+    { url: '/1.jpg', desc: 'one' },
+    { url: '/2.jpg', desc: 'two' },
+    { url: '/3.jpg', desc: 'three' },
+  ]
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
+    setCurrentIndex(newIndex)
+  }
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1
+    const newIndex = isLastSlide ? 0 : currentIndex + 1
+    setCurrentIndex(newIndex)
+  }
+
+  const goToSlide = slideIndex => {
+    setCurrentIndex(slideIndex)
+  }
+
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      {/* Left Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            {currentIndex === slideIndex ? (
+              <RxDotFilled className="text-red-400" />
+            ) : (
+              <RxDotFilled />
+            )}
+          </div>
+        ))}
+      </div>
+      <p className="text-center">{slides[currentIndex].desc}</p>
+    </div>
+  )
+}
+
+export default page
